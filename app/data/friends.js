f20933f22392f78f98c60a04693c241e66804301
Dre' Williams <dre6will6@gmail.com>
2019-03-11 20:24:11 -0500

    Updated after app crashed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,96 +1,91 @@
 
-var finderArray = [
+var clientArray = [
     {
       clientName: "Apryl Jones",
-      clientEmail: "AprylJones@gmail.com",
       clientPhoto: "http://www3.pictures.zimbio.com/gi/Apryl+Jones+Bossip+Best+Dressed+List+Event+S0R1XO__RA9l.jpg",
       clientScores: [
-        2,
-        3,
-        5,
-        2,
-        1,
-        2,
-        5,
-        4,
-        4,
-        3, 
+        "2",
+        "3",
+        "5",
+        "2",
+        "1",
+        "2",
+        "5",
+        "4",
+        "4",
+        "3", 
         ]
     },
   
     {
       clientName: "Jada Pinket-Smith",
-      clientEmail: "jadapsmith@gmail.com",
       clientPhoto: "https://pmcdeadline2.files.wordpress.com/2019/02/jada-pinkett-smith-photo-credit-don-flood.jpeg?w=446&h=299&crop=1",
       clientScores: [
-        1,
-        3,
-        5,
-        3,
-        2,
-        2,
-        4,
-        5,
-        3,
-        3, 
+        "1",
+        "3",
+        "5",
+        "3",
+        "2",
+        "2",
+        "4",
+        "5",
+        "3",
+        "3", 
         ]
     },
   
     {
       clientName: "Regina Hall",
-      clientEmail: "reginahall@gmail.com",
       clientPhoto: "http://celebrityinsider.org/wp-content/uploads/2018/08/Regina-Hall-BlackDoctor.org_-e1535649031125.jpg",
       clientScores: [
-        3,
-        3,
-        5,
-        4,
-        2,
-        3,
-        5,
-        3,
-        4,
-        2, 
+        "3",
+        "3",
+        "5",
+        "4",
+        "2",
+        "3",
+        "5",
+        "3",
+        "4",
+        "2", 
         ]
     },
   
     {
       clientName: "Carmen Electra",
-      clientEmail: "Carmen.Electra@gmail.com",
       clientPhoto: "https://resize.indiatvnews.com/en/centered/oldbucket/715_431/entertainmenthollywood/carmen-electra-hot.jpg",
       clientScores: [
-        2,
-        3,
-        5,
-        2,
-        1,
-        2,
-        5,
-        3,
-        3,
-        3, 
+        "2",
+        "3",
+        "5",
+        "2",
+        "1",
+        "2",
+        "5",
+        "3",
+        "3",
+        "3", 
         ]
     },
   
     {
       clientName: "Mike Colter",
-      clientEmail: "mikecolter@gmail.com",
       clientPhoto: "https://ewedit.files.wordpress.com/2015/01/mike-colter_612x380_2.jpg?w=612",
       clientScores: [
-        3,
-        3,
-        5,
-        4,
-        1,
-        2,
-        5,
-        4,
-        5,
-        5, 
+        "3",
+        "3",
+        "5",
+        "4",
+        "1",
+        "2",
+        "5",
+        "4",
+        "5",
+        "5", 
         ]
     }
   
   ]
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = finderArray;
+  module.exports = clientArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,20 +13,45 @@ module.exports = function(app) {
   app.post("/api/survey", function(req, res) {
     //Our "survey" will respond to requests here at this point.
 
-    if (surveyData.length < 5) {
+
+    //if (surveyData.length < 5) {
       surveyData.push(req.body);
-      res.json(true);
-    }
+
+      var clientScore = req.body.scores;
+
+      var surveyName = "";
+      var surveyPhoto = "";
+      var totalDifference = 25;
+
+      for (var i = o ; i < clientData.length -1 ; i ++) {
+          var difference = 0;
+
+          for (var k = 0; k < clientScore.length; k++) {
+              difference += Math.abs(friendData[i].score[k] - clientScore[k]);k
+          }
+
+          if (difference <totalDifference) {
+              totalDifference = difference;
+              surveyName = friendData[i].name;
+              surveyPhoto = friendData[i].photo;
+          }
+      }
+
+      res.json({ ok: true, surveyName: surveyName, surveyPhoto: surveyPhoto });
+    });
+  };
+      //res.json(true);
+    //}
     //else {
       //Data.push(req.body);
       //res.json(false);
     //}
-  });
+  //});
 
-  app.post("/api/clear", function(req, res) {
+  //app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
-    surveyData.length = [];
+    //surveyData.length = [];
 
-    res.json({ ok: true });
-  });
-};
+    //res.json({ ok: true, surveyName: surveyName, surveyPhoto: surveyPhoto });
+  //});
+//};
